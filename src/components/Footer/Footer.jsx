bc1260d7fb9dc4f28f8860d4ffe6e76a7aba3e96
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-19 19:40:04 -0400

    chore: Add page layout and implement CSS reset
    This commit introduces several enhancements to the overall styling of the page. It includes:
    
    - Addition of new font types and colors
    - Implementation of global styles
    - Incorporation of a CSS reset to ensure style consistency across browsers

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
