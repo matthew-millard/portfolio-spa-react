98f6fbf0202338514c0eed5a1485b3f4eefdf503
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-22 12:16:18 -0400

    Feat: add contact component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import AboutMe from './AboutMe';
+import Contact from './Contact';
 import styles from './Main.module.css';
 
 function Main() {
   return (
     <main className={styles.main}>
-      <AboutMe />
+      {/* <AboutMe /> */}
+      <Contact />
     </main>
   );
 }
---
+++ src/utils/validator.js
@@ -0,0 +1,7 @@
+// Email validator
+export const isValidEmail = email => {
+  const validEmailRegex = RegExp(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+  return validEmailRegex.test(email);
+};
